e4282db322513436735aca4e47222d3b654571c1
Alberto Merinero @ MX3750006dc0458 <alberto_merinero@iecisa.com>
2019-02-06 12:18:14 +0100

    newtermcli_retro.sh

---
+++ newtermcli_retro.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIA=`date +%Y%m%d`
+if [ $1 ];
+then
+	DESTINO=$@
+else
+	echo "Error: Necesito saber al menos el destino. user@host."
+	exit 1
+fi
+read arg1 arg2 arg3 <<< "$DESTINO"
+if  [ -z $arg3 ]
+then 
+	USERIP=$arg1
+else
+	USERIP=$arg3
+fi
+COMANDO="cool-retro-term -p alberto -e script -c \"ssh $DESTINO\" $HOME/.termlogs/${DIA}_${USERIP}.log > /dev/null 2>&1 &"
+#COMANDO="cool-retro-term -e ssh $DESTINO"
+
+#if [ $2 ];
+#then
+#	CLAVE=$2
+#	COMANDO="gnome-terminal --window-with-profile=cliente -x script -a -f -q -c \"ssh -i $CLAVE $DESTINO\" $HOME/.termlogs/newtermcli_${DIA}_${DESTINO}.log"
+#else
+#	COMANDO="gnome-terminal --window-with-profile=cliente -x script -a -f -q -c \"ssh $DESTINO\" $HOME/.termlogs/${DIA}_${DESTINO}.log"
+
+#fi
+echo "Ejecutando: $COMANDO"
+eval ${COMANDO}
